aa9c286e49195af9ae5e6acf8c99857a073ae8e2
Cherrying55 <cherrying556@protonmail.com>
2022-11-30 05:35:08 -0300

    Quase funcional

--- index.mjs
+++ index.mjs
@@ -1,6 +1,8 @@
 import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
-import joi, { x } from 'joi';
+import joi, { equal, x } from 'joi';
+import dayjs from 'dayjs';
+
 dotenv.config();
 
 const app = express();
@@ -39,7 +41,7 @@ app.post('/participants', async (req,res) => {
 })
 
 app.get("/participants", async (req,res) => {
-    const usercollection = dbLivros.collection("participants");
+    const usercollection = db.collection("participants");
     try{
         const lista = await usercollection.find().toArray()
         res.send(lista);
@@ -51,13 +53,74 @@ app.get("/participants", async (req,res) => {
 })
 
 app.post("/messages", async (req,res) => {
+    const messagecollection = db.collection("messages");
     const messageschema = joi.object({
         to: joi.string().required(),
         text: joi.string().required(),
-        type: joi.string(),
+        type: joi.string().valid('message', 'private_message').required(),
     })
     const validation = messageschema.validate(req.body);
     if(validation.error){
+        res.sendStatus(422);
+    }
+    //validar o from agora, que é header
+    try{
+        const jatem = await messagecollection.findOne({name: req.headers.user})
+        if(jatem === true){
+            res.sendStatus(422);
+        }
+    }
+    catch (error) {
+        res.sendStatus(422);
+    }
+    let newmsg = {
+        to: req.body.to,
+        text: req.body.text,
+        type: req.body.type,
+        from: req.headers.user,
+        time: dayjs("HH:MM:SS"),
+    }
+    messagecollection.insertOne(newmsg);
+    res.sendStatus(201);
+})
+
+app.get('/messages', async (req,res) => {
+    const messagecollection = db.collection("messages");
+    const limit = req.query.limit;
+    const user = req.headers.user;
+    const allmessages = 0;
+    try{
+        const r = await messagecollection.find().toArray();
+        allmessages = r;
+    }
+    catch (error){
+      res.sendStatus(422);
+    }
+    allmessages.filter((message) => {
+        if(message.type === "message"|| (message.type === "private_message" && message.to === user)){
+            return true;
+        }
+    })
+        res.send(limit? allmessages.slice(0,limit) : allmessages);
+
+})
+
+app.post('/status', async(req,res) => {
+    const user = req.headers.user;
+    const usercollection = db.collection("participants");
+    try{
+       const find = await usercollection.findOne({name: user})
+       if(!find){
+        res.sendStatus(404)
+       }
+       else{
+        let lastStatus = Date.now();
+        //ver como atualizar;
+        find.lastStatus = lastStatus;
+        res.sendStatus(200);
+       }
+    }
+    catch (error){
         res.sendStatus(422);
     }
 })
